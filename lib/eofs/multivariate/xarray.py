09fb7f3b1bbd44eaeaeca5f6451a15c599a53691
Matthew Woods <mawo3010@colorado.edu>
2026-04-16 10:50:20 -0600

    Add multivariate xarray file and set up start of file framework

---
+++ lib/eofs/multivariate/xarray.py
@@ -0,0 +1,25 @@
+
+import numpy
+
+from eofs import xarray
+
+
+
+
+class MultivariateEOF:
+
+    def __init__():
+
+
+
+    def _merge_fields():
+
+
+
+    def eofs():
+
+
+
+    def eigenvalues():
+
+
